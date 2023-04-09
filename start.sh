cead10d3d7e6ea546ea645187211149acb74c98a
Dare? <117949920+luteg96@users.noreply.github.com>
2023-04-10 05:24:14 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ git clone https://github.com/luteg96/Mahiru-Shiina /Mahiru-Shiina
 cd /Mahiru-Shiina
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by @luteg96"
-python3 bot.py
+python3 web.py && python3 bot.py
