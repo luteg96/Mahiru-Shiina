55abef025adacc264eefec698aad13195a54eeed
Dare? <117949920+luteg96@users.noreply.github.com>
2023-04-10 04:00:21 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/LISA-KOREA/UPLOADER-BOT-V3 /UPLOADER-BOT-V3
-cd /UPLOADER-BOT-V3
+git clone https://github.com/luteg96/Mahiru-Shiina /Mahiru-Shiina
+cd /Mahiru-Shiina
 pip3 install -U -r requirements.txt
-echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by @LISA_FAN_KL"
+echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by @luteg96"
 python3 bot.py
